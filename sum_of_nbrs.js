ac98777d9d3f4b3760ea827df4087dae89723049
Gym Ishyaka II <gymishyakaii@Ishyakas-iMac.local>
2024-12-17 15:17:39 +0200

    function to sum up numbers

---
+++ sum_of_nbrs.js
@@ -0,0 +1,13 @@
+var summation = function (num) {
+    let result = 0;
+    
+    for (let i = 0; i<=num; i++){
+      
+      result = result + i;
+      
+    }
+    return result;
+    // Code here
+  }
+  
+  console.log(summation(8));
